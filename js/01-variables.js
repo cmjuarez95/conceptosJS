6484cdba775911ae3b1fb4b37350772086b68a3f
Mariano Juarez <cm.juarez.95@gmail.com>
2025-06-03 21:31:38 -0300

    se empezo con variables

---
+++ js/01-variables.js
@@ -0,0 +1,17 @@
+// comentario de una sola linea   ctrl+}
+
+
+/*   
+comentario de multiples
+lineas
+*/
+
+// crear una variable (let - const - var)
+
+let numero1; //no hace falta el ;
+
+let anio = 2025
+
+// mostrar valor de las variables por consola del navegador
+
+console.log(anio,numero1)
