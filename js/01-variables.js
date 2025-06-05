f8c901329de098cee4e51e6d2a9e7320ebd7681d
Mariano Juarez <cm.juarez.95@gmail.com>
2025-06-05 19:57:15 -0300

    agregamos un ej de variables constantes y variables creadas con let

--- js/01-variables.js
+++ js/01-variables.js
@@ -12,6 +12,11 @@ let numero1; //no hace falta el ;
 
 let anio = 2025
 
+const comision="c13"
+
 // mostrar valor de las variables por consola del navegador
 
-console.log(anio,numero1)
+console.log(anio,numero1,comision)
+
+document.writeln("<h1>Práctica JS comision: " + comision + "</h1>")
+document.writeln("<p>contenido de la variable año "+ anio +"</p>")
