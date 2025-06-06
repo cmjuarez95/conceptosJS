719a30a256292470fe41565c6838230fa64b6a07
Mariano Juarez <cm.juarez.95@gmail.com>
2025-06-05 21:33:28 -0300

    empezamos un ejercicio del tp

---
+++ js/ejercicios/02-TP1-ejercicio9.js
@@ -0,0 +1,9 @@
+/*9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)*/
+
+let frase = prompt("Ingresa una frase")
+
+console.log(frase)
+console.log(frase.length)
+console.log(frase.substring(0,3))
+console.log(frase.charAt(5))
